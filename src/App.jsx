affa85b5db3d792ccd00c80d7c3ee98baa971bca
dat tran <tranthanhdat.it@gmail.com>
2026-04-02 13:30:35 +0700

    Fix routing and icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage.jsx';
 import EventDetail from './pages/EventDetail.jsx';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/event/:id" element={<EventDetail />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
